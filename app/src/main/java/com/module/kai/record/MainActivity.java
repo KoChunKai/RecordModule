d1d74e18117b6b8f7e91c153ae31db7753c5f3ab
Ko Chun Kai <KoChunKai@gmail.com>
2015-11-20 16:51:10 +0800

    came preview and autoFocus

--- app/src/main/java/com/module/kai/record/MainActivity.java
+++ app/src/main/java/com/module/kai/record/MainActivity.java
@@ -1,16 +1,52 @@
 package com.module.kai.record;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    Camera myCamera;
+    SurfaceView previewSurfaceView;
+    SurfaceHolder previewSurfaceHolder;
+    boolean previewing = false;
+
+    private final String tag = "VideoServer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //screen always on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        previewSurfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
+        previewSurfaceHolder = previewSurfaceView.getHolder();
+        previewSurfaceHolder.addCallback(this);
+        previewSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        previewSurfaceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myCamera.autoFocus(new Camera.AutoFocusCallback() {
+                    @Override
+                    public void onAutoFocus(boolean success, Camera camera) {
+                        Log.d("autofocus", "boolean:" + success);
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -33,5 +69,64 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        myCamera = Camera.open();
+        setCameraDisplayOrientation(MainActivity.this, 0);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if(previewing){
+            myCamera.stopPreview();
+            previewing = false;
+        }
+
+
+        try {
+            myCamera.setPreviewDisplay(holder);
+            myCamera.startPreview();
+            previewing = true;
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        myCamera.stopPreview();
+        myCamera.release();
+        myCamera = null;
+        previewing = false;
+    }
+
+    public void setCameraDisplayOrientation(Activity activity, int cameraId) {
+
+        Camera.CameraInfo info = new Camera.CameraInfo();
+
+        Camera.getCameraInfo(cameraId, info);
+
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        myCamera.setDisplayOrientation(result);
     }
 }
